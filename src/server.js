9fc88a513647bca52f8430e58811386055185038
Ana Alonso <alonso147411@e.unavarra.es>
2025-07-10 16:23:01 +0200

    Primera versión de endpoint top10

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const fastify = require('fastify')({logger: true});
-
+const fastify = require('fastify')({ logger: true });
+const axios = require('axios'); // node
 
 const PORT = 5555;
+
+fastify.get('/chess/top10', async (request, reply) => {
+    try {
+        const response = await axios.get('https://lichess.org/api/player');
+        reply.send(response.data);
+    } catch (error) {
+        reply.status(500).send({ error: 'Error al obtener datos de Lichess' });
+    }
+});
 
 const start = async () => {
     try{
