0d83048049626b4657ef9acf49ecd8be36ae9fbb
Ana Alonso <alonso147411@e.unavarra.es>
2025-07-11 13:27:24 +0200

    Añadiendo schema a endpoint 1, top10

--- src/routes/endpointTop10.ts
+++ src/routes/endpointTop10.ts
@@ -1,10 +1,49 @@
 import axios from 'axios';
 
 const getTop10 = function(fastify:any){
-    fastify.get('/chess/top10', async (request:any, reply:any) => {
+    fastify.get('/chess/top10', {
+            schema: {
+            response: {
+                200: {
+                    type: "object",
+                    additionalProperties: {
+                        type: "array",
+                        items: {
+                            type: "object",
+                            properties: {
+                                id: { type: "string" },
+                                username: { type: "string" },
+                                perfs: { type: "object", additionalProperties: true }
+                            },
+                            required: ["id", "username", "perfs"]
+                        }
+                    }
+                },
+                    404: {
+                        type: "object",
+                        properties: {
+                          error: { type: "string" }
+                        },
+                        required: ["error"]
+                      },
+                      500: {
+                        type: "object",
+                        properties: {
+                          error: { type: "string" }
+                        },
+                        required: ["error"]
+                      }
+            }
+        }
+    },async (request:any, reply:any) => {
         try {
             const response = await axios.get('https://lichess.org/api/player');
+            if (!response.data || Object.keys(response.data).length === 0) {
+                return reply.status(404).send({ error: 'No top players found.' });
+            }
+
             reply.send(response.data);
+
         } catch (error) {
             reply.status(500).send({ error: 'Internal server error' });
         }
